a9df77ccccf6cc7c94352dd450adc457bd4ca398
Jon Berliner <j.s.berliner@gmail.com>
2014-11-04 15:00:11 -0500

    init commit - clone of noChoice_exp0

---
+++ custom.py
@@ -0,0 +1,156 @@
+# this file imports custom routes into the experiment server
+from flask import Blueprint, render_template, request, jsonify, Response, abort, current_app
+from jinja2 import TemplateNotFound
+from functools import wraps
+from sqlalchemy import or_
+
+from psiturk.psiturk_config import PsiturkConfig
+from psiturk.experiment_errors import ExperimentError
+from psiturk.user_utils import PsiTurkAuthorization, nocache
+
+# # Database setup
+from psiturk.db import db_session, init_db
+from psiturk.models import Participant
+from json import dumps, loads
+
+# for basic experiment setup
+from numpy import linspace, array
+
+# load the configuration options
+config = PsiturkConfig()
+config.load_config()
+config.SECREY_KEY = 'my_secret_key'
+myauth = PsiTurkAuthorization(config)  # if you want to add a password protect route use this
+
+# explore the Blueprint
+custom_code = Blueprint('custom_code', __name__, template_folder='templates', static_folder='static')
+
+import gpExperiment as gpe
+
+## LOAD GP STUFF INTO WORKSPACE
+@custom_code.route('/init_experiment', methods=['GET'])
+def init_experiment():
+    if not request.args.has_key('condition'):
+        raise ExperimentError('improper_inputs')  # i don't like returning HTML to JSON requests...  maybe should change this
+
+    CONDITION = int(request.args['condition'])
+    COUNTERBALANCE = int(request.args['counterbalance'])
+
+    ## FREE VARS
+    # experiment params
+    NROUND = 200
+    COST2DRILL = 30
+    STARTPOINTS = 0
+    # gp params
+    nX = 1028
+    X = linspace(0, 1, nX)
+    SIGVAR = 1.
+    NOISEVAR2 = 1e-7
+    NROUND = 200
+    NOBSPOOL = [2, 3, 4, 5, 6]
+    ND2 = 4
+    EDGEBUF = 0.05 # samples for 2sams wont be closer than EDGEBUF from screen edge
+    D2POOL = [(1.-(EDGEBUF*2.))/(2.**(n+1)) for n in xrange(ND2)]
+    # made with numpy.random.randint(4294967295, size=20)  # (number is max allowed on amazon linux)
+    RNGSEEDPOOL =\
+        array([1903799985, 1543581047, 1218602148,  764353219, 1906699770,
+                951675775, 2101131205, 1792109879,  781776608, 2388543424,
+            2154736893, 2773127409, 3304953852,  678883645, 3097437001,
+            3696226994,  242457524,  991216532, 2747458246, 2432174005])
+    LENSCALEPOWSOF2 = [2., 4., 6.]
+    LENSCALEPOOL = [1./2.**n for n in LENSCALEPOWSOF2]
+    ## END FREE VARS
+    LENSCALE = LENSCALEPOOL[CONDITION]
+    RNGSEED = RNGSEEDPOOL[COUNTERBALANCE]
+    # LENSCALE = LENSCALEPOOL[CONDITION % len(LENSCALEPOOL)]
+    # RNGSEED = RNGSEEDPOOL[CONDITION % len(RNGSEEDPOOL)]
+
+    experParams = {'x': X,
+                   'lenscale': LENSCALE,
+                   'sigvar': SIGVAR,
+                   'noisevar2': NOISEVAR2,
+                   'rngseed': RNGSEED,
+                   'nRound': NROUND,
+                   'nObsPool': NOBSPOOL,
+                   'd2Pool': D2POOL,
+                   'edgeBuf': EDGEBUF}
+
+    subParams = gpe.make_experiment(**experParams)
+    # bundle response to send
+    resp = {}
+    for f in subParams:
+        try:
+            resp[f] = subParams[f].tolist()
+        except:
+            resp[f] = subParams[f]
+
+    for f in experParams:
+        try:  # convet numpy array to list if possible
+            resp[f] = experParams[f].tolist()
+        except:
+            resp[f] = experParams[f]
+
+    resp['round'] = -1
+    resp['d2i'] = -1
+    resp['nRound'] = NROUND
+    resp['cost2drill'] = COST2DRILL
+    resp['startPoints'] = STARTPOINTS
+
+    return jsonify(**resp)
+
+
+@custom_code.route('/get_nextTrial', methods=['GET'])
+def get_nextTrial():
+    # args:
+    #   lenscale
+    #   nObs
+    #   d2locsX
+    #   d2locsY
+    args = request.args
+    lenscale = float(args['lenscale'])
+    nObs = float(args['nObs'])
+    if nObs==2:
+        d2locsX = args['d2locsX']
+        d2locsY = args['d2locsY']
+        d2locsX = loads(d2locsX)
+        d2locsY = loads(d2locsY)
+        d2locsX = map(float, d2locsX)
+        d2locsY = map(float, d2locsY)
+        d2locsX = array(d2locsX)
+        d2locsY = array(d2locsY)
+    else:
+        d2locsX = None
+        d2locsY = None
+
+    nX = 1028
+    X = linspace(0, 1, nX)
+    SIGVAR = 1.
+    NOISEVAR2 = 1e-7
+
+    thisTri = gpe.make_trial(nObs, X, lenscale, SIGVAR, NOISEVAR2, d2locsX, d2locsY)
+
+    resp = {'sample': thisTri['sample'].tolist(),
+            'xObs': thisTri['xObs'].tolist(),
+            'yObs': thisTri['yObs'].tolist()}
+
+    return jsonify(**resp)
+
+
+###########################################################
+#  serving warm, fresh, & sweet custom, user-provided routes
+#  add them here
+###########################################################
+
+#----------------------------------------------
+# example custom route
+#----------------------------------------------
+@custom_code.route('/my_custom_view')
+def my_custom_view():
+        try:
+                return render_template('custom.html')
+        except TemplateNotFound:
+                abort(404)
+
+
+#  # set the secret key.  keep this really secret:
+#  current_app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
---
+++ gpExperiment.py
@@ -0,0 +1,104 @@
+import numpy as np
+from numpy.random import RandomState, randint
+from gpHelpers_cy import K_se, condition, sample
+
+isInt = lambda x: x == round(x)
+
+## EXAMPLE PARAMS
+DEMO = {}
+DEMO['nObsPool'] = [2, 3, 4, 5, 6]
+DEMO_ND2 = 5  # number of dists bt 2 points when 2 sams shown
+DEMO['d2Pool'] = [1./2.**(n+1) for n in xrange(DEMO_ND2)]  # dists bt 2 sams when 2 shown
+DEMO['nRound'] = 200  # n round in experiment
+DEMO['rngseed'] = 4245342523
+DEMO_NX = 1028
+DEMO['x'] = np.linspace(0, 1, DEMO_NX)
+DEMO['lenscale'] = 0.05
+DEMO['sigvar'] = 1.
+DEMO['noisevar2'] = 1e-7
+DEMO['edgeBuf'] = 0.05
+# get demo experiment with:
+# demoExp = gpExperiment.make_experiment(**gpExperiment.DEMO)
+## END EXAMPLE PARAMS
+
+
+# BEGIN DEFS
+def _make_nObsQueue(nObsPool, nRound, rng):
+    nPerNsam = nRound / len(nObsPool)
+    assert isInt(nPerNsam)
+    nObs_pool = np.repeat(nObsPool, nPerNsam)
+    nObs_order = rng.permutation(range(nRound))
+    nObs_queue = nObs_pool[nObs_order]
+
+    return nObs_queue
+
+
+def _make_d2locsQueue(d2pool, edgeBuf, nPerD2, nPerNSam, rng):
+    nD2 = len(d2pool)
+    L1rawpool = np.linspace(0+edgeBuf, 1-edgeBuf, nPerD2)  # linspace of loc1 (will be scale in the for-loop)
+
+    D2inds = np.repeat(range(nD2), nPerD2)  # indices of dists bt two points used in experiment
+    assert len(D2inds) == nPerNSam
+    L1inds = np.tile(range(nPerD2), nD2)  # indices of location one of two points
+    D2L1inds = np.vstack([D2inds, L1inds]).T
+    rng.shuffle(D2L1inds)
+
+    d2locs_queueX = np.ones([len(D2inds), 2])*np.nan  # init
+    d2locs_queueY = np.ones([len(D2inds), 2])*np.nan  # init
+    for iv, ii in enumerate(D2L1inds):
+        id2 = ii[0]  # get IND OF dist bt 2 points for this trial
+        il1 = ii[1]  # get IND OF location of point 1 for this trial
+        d0 = d2pool[id2]  # get dist bt 2 points
+        loc1 = L1rawpool[il1] * (1.-d0)  # get loc 1
+        d2locs_queueX[iv, 0] = loc1
+        d2locs_queueX[iv, 1] = loc1 + d0  # get location 2
+        d2locs_queueY[iv, :] = rng.randn(2)  # get y-vals for these locations
+
+    return {'d2locs_queueX': d2locs_queueX,
+            'd2locs_queueY': d2locs_queueY,
+            'D2L1inds': D2L1inds}
+
+
+def make_trial(nObs, X, lenscale, sigvar, noisevar2, d2locsX, d2locsY):
+    nX = len(X)
+    if nObs == 2:
+        xObs = d2locsX
+        yObs = d2locsY
+        post_gp = condition(X, xObs, yObs, lenscale, sigvar, noisevar2)
+        sam = sample(X, post_gp['mu'], post_gp['covmat'], noisevar2)
+        iObs = None
+    else:
+        # mu_prior will always be assumed to be the zero-mean prior
+        mu_prior = np.zeros_like(X)
+        # covmat_prior is sqexp kernel w lenscale LENSCALE and signal variance SIGVAR)
+        covmat_prior = K_se(X, X, lenscale, sigvar)  # get covmat for prior
+        sam = sample(X, mu_prior, covmat_prior, noisevar2)
+        iObs = randint(nX, size=nObs)
+        xObs = X[iObs]
+        yObs = sam[iObs]
+
+    return {'sample': sam,
+            'xObs': xObs,
+            'yObs': yObs,
+            'iObs': iObs}
+
+
+def make_experiment(nRound, nObsPool, d2Pool, edgeBuf, x, lenscale, sigvar, noisevar2, rngseed):
+    rng = RandomState(rngseed)
+    nPerNSam = nRound / len(nObsPool)
+    assert isInt(nPerNSam)
+    nD2 = len(d2Pool)
+    nPerD2 = nPerNSam / nD2
+    assert isInt(nPerD2)
+
+    nObsQueue = _make_nObsQueue(nObsPool, nRound, rng)
+    dist2stuff = _make_d2locsQueue(d2Pool, edgeBuf, nPerD2, nPerNSam, rng)
+    d2locsQueueX = dist2stuff['d2locs_queueX']
+    d2locsQueueY = dist2stuff['d2locs_queueY']
+    D2L1Inds = dist2stuff['D2L1inds']
+
+    return {'nObsQueue': nObsQueue,
+            'd2locsQueueX': d2locsQueueX,
+            'd2locsQueueY': d2locsQueueY,
+            'D2L1Inds': D2L1Inds}
+
